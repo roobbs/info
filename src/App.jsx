9dca73e3d9b98a76a04460c50344d491fee77b03
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-05-19 20:53:27 -0600

    Added structure and style of home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-// import "./styles/App.css";
+import "./styles/App.css";
 import Projects from "./components/Projects";
 import Header from "./components/Header";
-import Stack from "./components/Stack";
+import Name from "./components/name";
 
 export default function App() {
   return (
     <div className="page">
       <Header />
+      <Name />
       <Projects />
-      <Stack />
     </div>
   );
 }
---
+++ src/components/Name.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/Name.css";
+import Stack from "./Stack";
+
+export default function Name() {
+  return (
+    <div className="home">
+      <div className="homeName">
+        <div>
+          <div className="text">Hi, I'm Carlos Salmerón</div>
+          <div className="boldText">Full Stack Developer</div>
+          <div className="text">Another text</div>
+        </div>
+        <div className="picture">FOTO</div>
+      </div>
+      <div className="homeButtons">
+        <a
+          className="homeBtn"
+          href="https://github.com/roobbs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        <a
+          className="homeBtn"
+          href="https://github.com/roobbs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Linkedin
+        </a>
+      </div>
+      <Stack />
+    </div>
+  );
+}
