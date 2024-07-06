c5a28a4b534712b5dcc59e26d95e420159eda269
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-05 19:04:31 -0600

    Changed Projects.jsx structure

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,39 +10,43 @@ export default function Projects() {
   return (
     <section className="projectsSection" id="projects">
       <Title title={"Projects"} />
-      <div className="projectType">
-        Web Applications <div className="projectPoint">.</div>{" "}
+      <div className="projectTypeContainer">
+        <div className="projectType">
+          Web Applications <div className="projectPoint">.</div>
+        </div>
+        <div className="projects">
+          {webApps.map((project, index) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              view={project.view}
+              mobileView={project.mobileView}
+              desc={project.description}
+              language={project.language}
+              repo={project.repo}
+              website={project.website}
+              reverse={index % 2 === 0}
+            />
+          ))}
+        </div>
       </div>
-      <div className="projects">
-        {webApps.map((project, index) => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            view={project.view}
-            mobileView={project.mobileView}
-            desc={project.description}
-            language={project.language}
-            repo={project.repo}
-            website={project.website}
-            reverse={index % 2 === 0}
-          />
-        ))}
-      </div>
-      <div className="projectType">
-        Mobile Apps<div className="projectPoint">.</div>
-      </div>
-      <div>
-        {mobileApps.map((project, index) => (
-          <MobileProjectCard
-            key={project.id}
-            title={project.title}
-            view={project.view}
-            desc={project.description}
-            language={project.language}
-            repo={project.repo}
-            reverse={index % 2 === 0}
-          />
-        ))}
+      <div className="projectTypeContainer">
+        <div className="projectType">
+          Mobile Apps<div className="projectPoint">.</div>
+        </div>
+        <div className="projects">
+          {mobileApps.map((project, index) => (
+            <MobileProjectCard
+              key={project.id}
+              title={project.title}
+              view={project.view}
+              desc={project.description}
+              language={project.language}
+              repo={project.repo}
+              reverse={index % 2 === 0}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
